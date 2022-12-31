1365b2aca76628b5e2ab7679bd5ee65fa32c9418
wanghao8080 <wanghao@ninghao.net>
2022-12-31 14:45:35 +0800

    在 React 组件中显示列表

--- src/post/index/post-index.tsx
+++ src/post/index/post-index.tsx
@@ -8,7 +8,15 @@ type PostIndexProps = {};
 /**
  * 状态类型
  */
-type PostIndexState = {};
+type PostListItem = {
+  id: number;
+  content: string;
+  author: string;
+};
+
+type PostIndexState = {
+  postList: Array<PostListItem>;
+};
 
 /**
  * 组件
@@ -22,13 +30,39 @@ class PostIndex extends Component<PostIndexProps, PostIndexState> {
   /**
    * 组件状态
    */
-  state: PostIndexState = {};
+  state: PostIndexState = {
+    postList: [
+      {
+        id: 1,
+        content: '故人西辞黄鹤楼，烟花三月下扬州',
+        author: '李白',
+      },
+      {
+        id: 2,
+        content: '好雨知时节，当春乃发生',
+        author: '杜甫',
+      },
+      {
+        id: 3,
+        content: '浔阳江头夜送客，枫叶荻花秋瑟瑟',
+        author: '白居易',
+      },
+    ],
+  };
 
   /**
    * 渲染
    */
   render() {
-    return <div>PostIndex</div>;
+    const posts = this.state.postList.map((post) => {
+      return (
+        <div key={post.id}>
+          {post.content} - <small>{post.author}</small>
+        </div>
+      );
+    });
+
+    return <div>{posts}</div>;
   }
 }
 
