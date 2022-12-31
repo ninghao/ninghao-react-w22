9df8fec66e435cb8383d13287394526d72133713
wanghao8080 <wanghao@ninghao.net>
2022-12-31 11:57:15 +0800

    JSX：React 元素的友好表达方式

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,18 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-const element = React.createElement(
-  'div',
-  { className: 'container' },
-  React.createElement('h1', { className: 'title' }, '宁皓网'),
+// const element = React.createElement(
+//   'div',
+//   { className: 'container' },
+//   React.createElement('h1', { className: 'title' }, '宁皓网'),
+// );
+
+const name = '宁皓网';
+
+const element = (
+  <div className="container">
+    <h1 className="title">{name}</h1>
+  </div>
 );
 
 console.log(element);
