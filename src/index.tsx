d1346cb4a24d18ac3a84b163086d09c309f47408
wanghao8080 <wanghao@ninghao.net>
2022-12-31 11:52:28 +0800

    React 元素：构成应用界面的最小单元

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,16 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-root.render(<h1>宁皓网</h1>);
+const element = React.createElement(
+  'div',
+  { className: 'container' },
+  React.createElement('h1', { className: 'title' }, '宁皓网'),
+);
+
+console.log(element);
+
+root.render(element);
