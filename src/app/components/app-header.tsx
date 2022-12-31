e93b22cf6ccc53996d48d3cff588a49efce8fa86
wanghao8080 <wanghao@ninghao.net>
2022-12-31 13:33:40 +0800

    React 组件的属性（props）

--- src/app/components/app-header.tsx
+++ src/app/components/app-header.tsx
@@ -1,11 +1,19 @@
-const AppHeader = () => {
-  const name = '宁皓网';
+type AppHeaderProps = {
+  name: string;
+};
+
+const AppHeader = (props: AppHeaderProps) => {
+  const { name } = props;
 
   return (
     <div className="app-header">
       <h1 className="title">{name}</h1>
     </div>
   );
+};
+
+AppHeader.defaultProps = {
+  name: '宁皓网',
 };
 
 export default AppHeader;
