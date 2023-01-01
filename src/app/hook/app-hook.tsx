58abb9af958ddf71a187aa7a851ee5fb0d7f11ee
wanghao8080 <wanghao@ninghao.net>
2023-01-01 08:05:17 +0800

    useState：在函数式组件里使用状态

--- src/app/hook/app-hook.tsx
+++ src/app/hook/app-hook.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './app-hook.css';
 
 /**
@@ -9,7 +10,33 @@ type AppHookProps = {};
  * 组件
  */
 const AppHook = (props: AppHookProps) => {
-  return <div className="app-hook">AppHook</div>;
+  const [emoji, setEmoji] = useState('🦖');
+  const [quantity, setQuantity] = useState(5);
+
+  const onClickIncrease = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const onClickDecrease = () => {
+    setQuantity(quantity - 1);
+  };
+
+  const onClickEmoji = () => {
+    setEmoji(emoji === '🌲' ? '🦖' : '🌲');
+  };
+
+  return (
+    <div className="app-hook">
+      <h1>
+        {emoji} <br /> {quantity}
+      </h1>
+      <button onClick={onClickEmoji}>表情符号</button>
+      <div>
+        <button onClick={onClickIncrease}>+</button>
+        <button onClick={onClickDecrease}>-</button>
+      </div>
+    </div>
+  );
 };
 
 export default AppHook;
