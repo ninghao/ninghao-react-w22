19a45514881f60ee3b7994f27e2a137638227550
wanghao8080 <wanghao@ninghao.net>
2023-01-02 14:38:40 +0800

    改造 AppHeader 组件

--- src/app/components/app-header.tsx
+++ src/app/components/app-header.tsx
@@ -1,121 +1,41 @@
-import { Component, MouseEvent } from 'react';
-import AppButton from 'app/components/app-button';
-import { AppContext } from 'app/app';
-import lightIcon from 'app/icons/light.svg';
-import darkIcon from 'app/icons/dark.svg';
-import './app-header.css';
 import { Link, NavLink } from 'react-router-dom';
 import { appConfig } from 'app/app.config';
 import addIcon from 'app/icons/add.svg';
+import accountIcon from 'app/icons/account.svg';
+import './app-header.css';
 
-type AppHeaderProps = {
-  name: string;
-};
+/**
+ * 属性类型
+ */
+type AppHeaderProps = {};
 
-type AppHeaderState = {
-  emoji: string;
-  isLoggedIn: boolean;
-};
-
-class AppHeader extends Component<AppHeaderProps, AppHeaderState> {
-  constructor(props: AppHeaderProps) {
-    super(props);
-
-    this.state = {
-      emoji: '🌴',
-      isLoggedIn: false,
-    };
-  }
-
-  login = () => {
-    this.setState({ isLoggedIn: true });
-  };
-
-  logout = () => {
-    this.setState({ isLoggedIn: false });
-  };
-
-  // state: AppHeaderState = {
-  //   emoji: '🌴',
-  // };
-
-  static defaultProps = {
-    name: '宁皓网',
-  };
-
-  changeEmoji = (event: MouseEvent) => {
-    console.log('点击标题', event);
-
-    this.setState((state, _) => {
-      return {
-        emoji: state.emoji === '🌴' ? '🏕' : '🌴',
-      };
-    });
-  };
-
-  render() {
-    const { emoji, isLoggedIn } = this.state;
-
-    const loginAction = (
-      <AppButton variant="primary" onClick={this.login}>
-        登录
-      </AppButton>
-    );
-
-    const logoutAction = (
-      <AppButton variant="bordered" onClick={this.logout}>
-        退出
-      </AppButton>
-    );
-
-    return (
-      <AppContext.Consumer>
-        {({ theme, setTheme }) => (
-          <div className="app-header">
-            <h1
-              onClick={this.changeEmoji}
-              className="title"
-              style={{ cursor: 'pointer', fontSize: '24px' }}
-            >
-              <NavLink to="/">{appConfig.appName}</NavLink> {emoji}
-            </h1>
-            <nav>
-              <NavLink to="/about">关于</NavLink>
-              <NavLink to="/posts">内容</NavLink>
-            </nav>
-            <div className="content">
-              {isLoggedIn && <div>你好，开发者！</div>}
-              {isLoggedIn ? logoutAction : loginAction}
-              <div>
-                {theme === 'light' ? (
-                  <img
-                    src={lightIcon}
-                    alt=""
-                    onClick={() => setTheme!('dark')}
-                  />
-                ) : (
-                  <img
-                    src={darkIcon}
-                    alt=""
-                    onClick={() => setTheme!('light')}
-                  />
-                )}
-              </div>
-              <div>
-                <Link to="/create">
-                  <img src={addIcon} alt="添加内容" />
-                </Link>
-              </div>
-            </div>
-          </div>
-        )}
-      </AppContext.Consumer>
-    );
-  }
-}
-
-AppHeader.defaultProps = {
-  name: '宁皓网',
+/**
+ * 组件
+ */
+const AppHeader = (props: AppHeaderProps) => {
+  return (
+    <div className="app-header">
+      <h1 className="title">
+        <NavLink to="/">{appConfig.appName}</NavLink> 🌴
+      </h1>
+      <nav>
+        <NavLink to="/about">关于</NavLink>
+        <NavLink to="/posts">内容</NavLink>
+      </nav>
+      <div className="content">
+        <div>
+          <Link to="/create">
+            <img src={addIcon} alt="添加内容" />
+          </Link>
+        </div>
+        <div>
+          <Link to="/login">
+            <img src={accountIcon} alt="用户登录" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AppHeader;
