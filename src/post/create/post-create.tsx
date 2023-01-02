689cef4fd092108970891e7efb89df1b9f0d8133
wanghao8080 <wanghao@ninghao.net>
2023-01-02 09:47:38 +0800

    在 React 应用里请求接口发布内容（视图）

--- src/post/create/post-create.tsx
+++ src/post/create/post-create.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { apiHttpClient } from 'app/app.service';
+import { useEffect, useState } from 'react';
 
 /**
  * 属性类型
@@ -13,7 +14,46 @@ const PostCreate = (props: PostCreateProps) => {
     document.title = '创建内容 - 宁皓网';
   });
 
-  return <div>PostCreate</div>;
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const createPost = async () => {
+    if (title && content) {
+      try {
+        await apiHttpClient.post('posts', { title, content });
+        setTitle('');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="标题"
+          value={title}
+          onChange={(event) => {
+            setTitle(event.currentTarget.value);
+          }}
+        />
+      </div>
+      <div>
+        <textarea
+          placeholder="正文"
+          value={content}
+          onChange={(event) => {
+            setContent(event.currentTarget.value);
+          }}
+        ></textarea>
+      </div>
+      <div>
+        <button onClick={createPost}>发布</button>
+      </div>
+    </div>
+  );
 };
 
 export default PostCreate;
