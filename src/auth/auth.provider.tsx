3b35f3bc73446605ad2e54695d25ebbef51afb36
wanghao8080 <wanghao@ninghao.net>
2023-01-03 10:40:35 +0800

    在 React 应用里存储与恢复用户登录状态

--- src/auth/auth.provider.tsx
+++ src/auth/auth.provider.tsx
@@ -1,8 +1,10 @@
+import { getStorage, setStorage, StorageKey } from 'app/app.service';
 import {
   createContext,
   Dispatch,
   ReactNode,
   SetStateAction,
+  useEffect,
   useState,
 } from 'react';
 
@@ -27,6 +29,20 @@ export const AuthContext = createContext<AuthContextValue>({});
  */
 const AuthProvider = (props: AuthProviderProps) => {
   const [currentUser, setCurrentUser] = useState<CurrentUser>();
+
+  useEffect(() => {
+    if (currentUser !== undefined) {
+      setStorage(StorageKey.currentUser, currentUser);
+    } else {
+      const currentUserFromStorage = getStorage<CurrentUser>(
+        StorageKey.currentUser,
+      );
+
+      if (currentUserFromStorage) {
+        setCurrentUser(currentUserFromStorage);
+      }
+    }
+  }, [currentUser]);
 
   return (
     <AuthContext.Provider value={{ currentUser, setCurrentUser }}>
